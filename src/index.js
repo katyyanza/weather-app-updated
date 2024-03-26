fc9599e8134bd5fcb2e201e770189e926258356b
katyyanza <154392802+katyyanza@users.noreply.github.com>
2024-03-26 17:17:05 -0400

    created an array for multiple days and created a function called displayForecast

--- src/index.js
+++ src/index.js
@@ -59,7 +59,33 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+  <div class="weather-forecast-day">
+    <div class="weather-forecast-date"> ${day}</div>
+    <div class="weather-forecast-icon">⛅️ </div>
+    <div class="weather-forecast-temperatures">
+      <span class="weather-forecast-temperature">
+        <strong> 16°</strong>
+      </span>
+      <span class="weather-forecast-temperature">9°</span>
+    </div>
+  </div>
+`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Quito");
+
+displayForecast();
